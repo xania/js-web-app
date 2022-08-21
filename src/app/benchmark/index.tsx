d81158526f82c907ba18283a6685acfa4902aecb
xania <34893682+xania@users.noreply.github.com>
2022-08-21 22:11:39 +0200

    upgrade

--- src/app/benchmark/index.tsx
+++ src/app/benchmark/index.tsx
@@ -1,5 +1,5 @@
-import { TableStore, DataRow } from "./table-store";
-import { createContainer, render, useContext } from "@xania/view";
+import { TableStore } from "./table-store";
+import { render } from "@xania/view";
 import * as view from "@xania/view";
 const jsx = view.jsx.createElement;
 
@@ -87,14 +87,13 @@ function Jumbotron(props: JumbotronProps) {
 }
 
 function App() {
-  const rows = createContainer<DataRow>();
-  const store = new TableStore(rows);
+  const store = new TableStore();
   return (
     <div id="main">
       <div class="container">
         <Jumbotron store={store} />
         <table class="table table-hover table-striped test-data">
-          <tbody>{rows.map(<Row {...store} />)}</tbody>
+          <tbody>{store.view}</tbody>
         </table>
         <span
           class="preloadicon glyphicon glyphicon-remove"
@@ -102,35 +101,6 @@ function App() {
         ></span>
       </div>
     </div>
-  );
-}
-
-interface RowProps {
-  select({ node: Node });
-  delete({ node: Node });
-}
-
-function Row(props: RowProps) {
-  const $ = useContext<DataRow>();
-
-  return (
-    <tr className={$(`className`)}>
-      <td class="col-md-1">{$("id")}</td>
-      <td class="col-md-4">
-        <a class="lbl" click={props.select}>
-          {$("label")}
-        </a>
-      </td>
-      <td class="col-md-1">
-        <a class="remove" click={props.delete}>
-          <span
-            class="remove glyphicon glyphicon-remove"
-            aria-hidden="true"
-          ></span>
-        </a>
-      </td>
-      <td class="col-md-6"></td>
-    </tr>
   );
 }
 
---
+++ src/app/benchmark/table-store.tsx
@@ -0,0 +1,207 @@
+import { createView, useContext, View } from "@xania/view";
+import * as xv from "@xania/view";
+const jsx = xv.jsx.createElement;
+
+export interface DataRow {
+  id: number;
+  label: string;
+  className?: string;
+}
+
+var adjectives = [
+  "pretty",
+  "large",
+  "big",
+  "small",
+  "tall",
+  "short",
+  "long",
+  "handsome",
+  "plain",
+  "quaint",
+  "clean",
+  "elegant",
+  "easy",
+  "angry",
+  "crazy",
+  "helpful",
+  "mushy",
+  "odd",
+  "unsightly",
+  "adorable",
+  "important",
+  "inexpensive",
+  "cheap",
+  "expensive",
+  "fancy",
+];
+var colours = [
+  "red",
+  "yellow",
+  "blue",
+  "green",
+  "pink",
+  "brown",
+  "purple",
+  "brown",
+  "white",
+  "black",
+  "orange",
+];
+var nouns = [
+  "table",
+  "chair",
+  "house",
+  "bbq",
+  "desk",
+  "car",
+  "pony",
+  "cookie",
+  "sandwich",
+  "burger",
+  "pizza",
+  "mouse",
+  "keyboard",
+];
+
+export class TableStore {
+  private counter = 1;
+  private data: DataRow[] = [];
+  public view: View<DataRow>;
+
+  constructor() {
+    this.view = createView<DataRow>(
+      <Row select={this.select} delete={this.delete} />
+    );
+  }
+
+  selected: DataRow | null = null;
+
+  run = () => {
+    const { view: container, data } = this;
+    step(0);
+    function step(idx: number) {
+      if (idx < data.length && idx < 500) {
+        data[idx].className = "danger";
+        if (idx > 0) {
+          data[idx - 1].className = null;
+        }
+        container.update(data);
+        setTimeout(function () {
+          step(idx + 1);
+        }, 0);
+      }
+    }
+  };
+
+  select = (e: { values: DataRow }) => {
+    const { selected, view: container, data } = this;
+    const values = e.values;
+    if (selected !== values) {
+      const newValues = values;
+      newValues.className = "danger";
+      if (selected) {
+        selected.className = null;
+      }
+      container.update(data);
+    }
+
+    this.selected = values;
+  };
+
+  delete = (e: { values: DataRow }) => {
+    const rowValues = e.values;
+    const index = this.data.indexOf(rowValues);
+    this.data.splice(index, 1);
+    this.view.removeAt(index);
+  };
+
+  private appendRows(count: number) {
+    let { counter, view: container, data } = this;
+    const offset = data.length;
+    for (let i = offset, len = offset + count; i < len; i++) {
+      data[i] = {
+        id: counter++,
+        label:
+          adjectives[_random(adjectives.length)] +
+          " " +
+          colours[_random(colours.length)] +
+          " " +
+          nouns[_random(nouns.length)],
+        className: null,
+      };
+    }
+
+    container.update(data);
+    this.counter = counter;
+  }
+
+  create1000Rows = (): void => {
+    this.clear();
+    this.appendRows(2);
+  };
+  create10000Rows = (): void => {
+    this.clear();
+    this.appendRows(10000);
+  };
+  append1000Rows = (): void => {
+    this.appendRows(2);
+  };
+
+  updateEvery10thRow = (): void => {
+    const { view: container, data } = this;
+
+    for (let i = 0; i < data.length; i += 10) {
+      data[i].label += " !!!";
+    }
+    container.update(data);
+  };
+  clear = (): void => {
+    this.view.clear();
+    this.selected = null;
+    this.data.length = 0;
+  };
+  swapRows = (): void => {
+    const { data } = this;
+    if (data.length > 4) {
+      const tmp = data[1];
+      data[1] = data[4];
+      data[4] = tmp;
+      this.view.move(1, 4);
+      this.view.move(3, 1);
+    }
+  };
+}
+
+interface RowProps {
+  select({ values: DataRow });
+  delete({ values: DataRow });
+}
+
+function Row(props: RowProps) {
+  const $ = useContext<DataRow>();
+
+  return (
+    <tr className={$(`className`)}>
+      <td class="col-md-1">{$("id")}</td>
+      <td class="col-md-4">
+        <a class="lbl" click={props.select}>
+          {$("label")}
+        </a>
+      </td>
+      <td class="col-md-1">
+        <a class="remove" click={props.delete}>
+          <span
+            class="remove glyphicon glyphicon-remove"
+            aria-hidden="true"
+          ></span>
+        </a>
+      </td>
+      <td class="col-md-6"></td>
+    </tr>
+  );
+}
+
+function _random(max) {
+  return Math.round(Math.random() * 1000) % max;
+}
